ef5f9c11399ecaa5180205714d7fdfbd01be92f2
Arne Franken <franken@adobe.com>
2023-12-21 21:08:55 +0100

    Day01 Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        input
+            .asSequence()
+            .filterNot(String::isBlank)
+            .map(String::toList)
+            .map {
+                var first: Int? = null
+                var last: Int? = null
+                it
+                  .filter { it.isDigit() }
+                  .forEach {
+                        if (first == null) {
+                            first = it.digitToInt()
+                        }
+                        last = it.digitToInt()
+                    }
+                "$first$last".toInt()
+            }
+            .forEach {
+                result += it
+            }
+        return result
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +30,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
-    part1(input).println()
+    check(part1(input) == 56049)
+
     part2(input).println()
 }
